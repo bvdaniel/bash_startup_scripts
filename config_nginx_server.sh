bea612ffddc4c83de98f60afa38ee8b3f4d403c2
Daniel Esteban Beltrán Varas <70292984+bvdaniel@users.noreply.github.com>
2022-08-21 12:17:02 -0400

    Create config_nginx_server.sh

---
+++ config_nginx_server.sh
@@ -0,0 +1,12 @@
+cat <<EOF> /etc/nginx/sites-available/orienta_web
+server{
+listen 80;
+listen [::];
+root /var/www/orienta_web;
+index index.html
+server_name orienta-webserver;
+location / {
+try_files $uri $uri/ =404;
+}
+}
+EOF
